1981d14436c2ce08ecbbc93ce8a50ec2b2ceecd9
yasserelhor <elhoryasser6@gmail.com>
2023-11-28 10:07:29 -0800

    adding the thing

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i == 00:
-        print(f"{i:02}", end='')
-    else:
-        print(f", {i:02}", end='')
+    print(f"{', ' if i > 0 else ''}{i:02}", end='')
 print()
