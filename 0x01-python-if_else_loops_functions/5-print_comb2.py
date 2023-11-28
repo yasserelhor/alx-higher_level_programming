8f3c8da83f3c3548aec21e2c354dfc128e2b50ef
yasserelhor <elhoryasser6@gmail.com>
2023-11-28 10:15:16 -0800

    adding the thing

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{', ' if i > 0 else ''}{i:02}", end='')
-print()
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}, ".format(i), end='')
