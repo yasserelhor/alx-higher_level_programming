a8af53a14465d2035b0241e0ea3db648d68c4b85
yasserelhor <elhoryasser6@gmail.com>
2023-11-30 12:39:05 -0800

    adding the thing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,18 +4,21 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv)
-
+    one = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
+    two = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
+    tree = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
+    four = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
     if count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] == '+':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], one))
     elif sys.argv[2] == '-':
-        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], two))
     elif sys.argv[2] == '*':
-        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], tree))
     elif sys.argv[2] == '/':
-        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], foor))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
