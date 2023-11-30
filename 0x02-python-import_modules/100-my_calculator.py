94b9cd91bb16b2de8bf8555cf1affd8dbf22571a
yasserelhor <elhoryasser6@gmail.com>
2023-11-30 12:52:56 -0800

    adding the thing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,18 +6,21 @@ if __name__ == "__main__":
     if count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    one = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
-    two = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-    tree = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-    four = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-    if sys.argv[2] == '+':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], one))
-    elif sys.argv[2] == '-':
-        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], two))
-    elif sys.argv[2] == '*':
-        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], tree))
-    elif sys.argv[2] == '/':
-        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], foor))
+
+    a = int(sys.argv[1])
+    ope = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if ope == '+':
+        result = calculator_1.add(a, b)
+    elif ope == '-':
+        result = calculator_1.sub(a, b)
+    elif ope == '*':
+        result = calculator_1.mul(a, b)
+    elif ope == '/':
+        result = calculator_1.div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
+    print("{} {} {} = {}".format(a, ope, b, result))
