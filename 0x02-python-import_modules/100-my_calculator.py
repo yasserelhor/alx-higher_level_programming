a6c7448c774069d3c2d0b9f011260ef218e49ff7
yasserelhor <elhoryasser6@gmail.com>
2023-11-30 12:41:58 -0800

    adding the thing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,15 +2,14 @@
 if __name__ == "__main__":
     import calculator_1
     import sys
-
     count = len(sys.argv)
+    if count != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
     one = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
     two = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
     tree = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
     four = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-    if count != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
     if sys.argv[2] == '+':
         print("{} + {} = {}".format(sys.argv[1], sys.argv[3], one))
     elif sys.argv[2] == '-':
