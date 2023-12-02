4bed9ef4b09944fc3281d0f2d51ea77ace6bc5b8
yasserelhor <elhoryasser6@gmail.com>
2023-12-02 09:43:39 -0800

    adding the thing

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,28 +1,27 @@
 #include "Python.h"
+
 /**
- * print_python_list_info - Prints information about python objects
- * @p: PyObject pointer to print info about
- * Compile with:
- * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared
- (* -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4
- (* 100-print_python_list_info.c
-*/
+ * print_python_list_info - Prints information about a Python List object.
+ * @p: Pointer to the PyObject representing the Python List.
+ */
+
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i, py_list_size;
-	PyObject *item;
+	Py_ssize_t list_size;
+	PyObject *list_item;
 	const char *item_type;
-	PyListObject *list_object_cast;
+	PyListObject *casted_list_object;
 
-	list_object_cast = (PyListObject *)p;
-	py_list_size = PyList_Size(p);
+	casted_list_object = (PyListObject *)p;
+	list_size = PyList_Size(p);
 
-	printf("[*] Size of the Python List = %d\n", (int) py_list_size);
-	printf("[*] Allocated = %d\n", (int)list_object_cast->allocated);
-	for (i = 0; i < py_list_size; i++)
+	printf("[*] Size of the Python List = %ld\n", list_size);
+	printf("[*] Allocated = %d\n", (int)casted_list_object->allocated);
+	
+	for (Py_ssize_t i = 0; i < list_size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		item_type = Py_TYPE(item)->tp_name;
-		printf("Element %d: %s\n", (int) i, item_type);
+		list_item = PyList_GetItem(p, i);
+		item_type = Py_TYPE(list_item)->tp_name;
+		printf("Element %ld: %s\n", i, item_type);
 	}
 }
