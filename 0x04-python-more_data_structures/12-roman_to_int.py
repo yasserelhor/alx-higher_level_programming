6353278e3193e318a52037914fbdbea3e0ee9987
yasserelhor <elhoryasser6@gmail.com>
2023-12-06 12:29:04 -0800

    adding the thing

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    dig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    number = 0
+
+    for i in reversed(roman_string):
+        number = dig[i]
+        total += number if total < number * 5 else -number
+    return total
