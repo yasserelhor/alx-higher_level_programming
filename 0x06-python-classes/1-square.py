7310a6e2af6a47c662e1ba956ce1019fa3ca2acd
yasserelhor <elhoryasser6@gmail.com>
2023-12-19 12:33:47 -0800

    adding the thing

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square Class def"""
+
+class Square:
+    """defining a square"""
+    def __init__(self, size):
+        """initializing a square"""
+        self.__size = size
