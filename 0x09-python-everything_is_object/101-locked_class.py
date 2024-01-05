17185f20a873b2d5d9f2f39ac7f84e75c4d13e7a
yasserelhor <elhoryasser6@gmail.com>
2024-01-05 05:55:07 -0800

    adding the thing

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+LockedClass Module
+
+Defines LockedClass with a single attribute 'first_name'.
+"""
+
+class LockedClass:
+    """
+    LockedClass Module
+
+    Defines LockedClass with a single attribute 'first_name'.
+    """
+    
+    
+    __slots__ = ["first_name"]
