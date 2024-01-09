df3801be6a290cbf4f58110169719b4340ff07b8
yasserelhor <elhoryasser6@gmail.com>
2024-01-09 17:04:28 +0100

    adding

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""
+Writes text to a file and returns the character count.
+
+Usage: char_count = write_file("filename.txt", "Your text here")
+"""
+
+def write_file(filename="", text=""):
+    """
+    Parameters: filename (str), text (str)
+    Returns: int (character count)
+    """
+    char = 0
+    for i in text:
+        char += 1
+    
+    with open(filename, 'w', encoding='UTF-8',) as F:
+        F.write(text)
+    
+    return char
