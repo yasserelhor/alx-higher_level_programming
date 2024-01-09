0effc4225146fcdbcd13f6e04c655c1fe61685d3
yasserelhor <elhoryasser6@gmail.com>
2024-01-09 16:31:45 +0100

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 
 def read_file(filename=""):
-    with open(filename, mode="r", encoding='utf-8') as myFile:
+    with open(filename, mode="r", encoding='UTF-8') as myFile:
         print(myFile.read(), end='')
