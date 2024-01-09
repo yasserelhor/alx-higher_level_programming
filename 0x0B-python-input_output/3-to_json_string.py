cb06a6820ee2d5c256d89058b4ab01b13ef97be0
yasserelhor <elhoryasser6@gmail.com>
2024-01-09 17:27:45 +0100

    you know agian

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """defines json function"""
-
-
 import json
-def to_json_string(my_obj):
-    """return dumps"""
 
+
+def to_json_string(my_obj):    
+    """return dumps"""
     return json.dumps(my_obj)
