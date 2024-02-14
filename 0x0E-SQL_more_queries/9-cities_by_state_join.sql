f277528a01dc95c81458d0fbfd7fdb8ab6994095
yasserelhor <elhoryasser6@gmail.com>
2024-02-14 16:35:48 +0100

    adding the thing

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 --lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name
-FROM states
-INNER JOIN cities
-ON states.id = cities.state_id;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
