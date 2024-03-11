09d81505bb1337ee8c75617eb33d59ddec722020
yasserelhor <elhoryasser6@gmail.com>
2024-03-11 13:12:24 +0000

    adding the thing

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const lan = args.length;
+if (lan === 0) {
+  console.log('No argument');
+} else if (lan === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
