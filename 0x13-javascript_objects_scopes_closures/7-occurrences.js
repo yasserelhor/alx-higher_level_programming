2a6ac3465860df8f087126d0e1d6bebfa6930b9c
yasserelhor <elhoryasser6@gmail.com>
2024-03-13 01:02:40 +0000

    adding the thing

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    return list.reduce((count, current) => current === searchElement
-      ? count + 1
-      : count, 0
-    );
-  }
+  return list.reduce((count, current) => current === searchElement
+    ? count + 1
+    : count, 0
+  );
+};
