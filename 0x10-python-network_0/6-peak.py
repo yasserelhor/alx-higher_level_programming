2c6aaf365eb1873a3bd4a1d27022ee60c6ad48a0
yasserelhor <elhoryasser6@gmail.com>
2024-03-27 14:06:14 +0000

    adding the thing

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,8 @@
 def find_peak(list_of_integers):
     """functrion"""
     if list_of_integers:
-        w = len(list_of_integers) - 1
         f = 0
+        w = len(list_of_integers) - 1
         while f < w:
             mid = (f+w)//2
             if list_of_integers[mid] > list_of_integers[mid + 1]:
