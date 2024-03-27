dd30556165259ccc43463060cf668d4fab6f2367
yasserelhor <elhoryasser6@gmail.com>
2024-03-27 13:56:28 +0000

    adding the thing

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this function finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """functrion"""
+    if list_of_integers:
+        w = len(list_of_integers)- 1
+        f = 0
+        while f < w:
+            mid = (f+w)//2
+            if list_of_integers[mid] > list_of_integers[mid + 1]:
+                w = mid
+            else:
+                f = mid+1
+        return list_of_integers[f]
