c7b0479d34e5a3ec7db6ee264c1afd48c22150ed
yasserelhor <elhoryasser6@gmail.com>
2024-03-28 04:25:26 +0000

    adding the thing

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this script fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with request.urlopen(url) as res:
+        data = res.read()
+        par_data = data.decode('utf-8')
+        r_type = type(data)
+        print(f"Body response:\n\t- type: {r_type}\n\t\
+- content: {data}\n\t- utf8 content: {par_data}")
