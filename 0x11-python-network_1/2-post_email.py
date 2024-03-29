81fc175d2273d61df3a7ee1f19fec4150d0a518d
yasserelhor <elhoryasser6@gmail.com>
2024-03-29 17:10:26 +0000

    adding the thing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""this script takes in a URL and an e_mail, sends
-   a POST request to the passed URL with the e_mail a
-   a parameter, and displays the body of the response (decoded in utf-8)"""
-import sys
-from urllib import request, parse
+"""This Script Sends a POST request to the URL passed as argument with
+    the email as a parameter also passed as argument,
+    displays the body of the repsonse(decoded in utf-8)
+"""
+from sys import argv
+import urllib.request as req
+import urllib.parse as parse
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    e_mail = sys.argv[2]
-    i3dadat = {"e_mail": e_mail}
-    i3dadat_utf = parse.urlencode(i3dadat).encode('utf-8')
-    req = request.Request(url, data=i3dadat_utf, method='POST')
-    with request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+    url = argv[1]
+    values = {'email': argv[2]}
+    email = parse.urlencode(values).encode("ascii")
+
+    request = req.Request(url, email)
+    with req.urlopen(request) as response:
+        page = response.read()
+        print(page.decode("utf-8"))
