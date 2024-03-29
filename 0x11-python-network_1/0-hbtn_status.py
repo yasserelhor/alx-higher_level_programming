548f6cfa35f6b2ecd13a866546ebb987c5b1bff7
yasserelhor <elhoryasser6@gmail.com>
2024-03-29 17:12:35 +0000

    adding the thing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""this script fetches https://alx-intranet.hbtn.io/status"""
-from urllib import request
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as req
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with request.urlopen(url) as res:
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as res:
         data = res.read()
-        par_data = data.decode('utf-8')
-        r_type = type(data)
-        print(f"Body response:\n\t- type: {r_type}\n\t\
-              - content: {data}\n\t- utf8 content: {par_data}")
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
